01526643dcc32a369e8aab36a3a1fb5f13a94b7c
Matias F. Acevedo <matias.acevedo56732@gmail.com>
2023-04-15 23:28:20 -0300

    agrego la funcion crearAlumno al gestor

--- gestorColegio.ts
+++ gestorColegio.ts
@@ -1,4 +1,5 @@
-
+const chalk = require('chalk');
+const { v4: uuidv4 } = require('uuid');
 const fs = require("fs")
 const readLineSync = require ("readline-sync")
 
@@ -8,42 +9,43 @@ import Materia from "./Materia";
 import Profesor from "./Profesor";
 import { clear, log } from "console";
 import Curso from "./Curso";
+import NotaPorMateria from "./notaPorMateria";
 
 // Se crean las materias de primer año:
-const naturales:Materia = new Materia("Naturales")
-const matematicas:Materia = new Materia("Matematicas")
-const fisica:Materia = new Materia("Fisica")
-const anatomia:Materia = new Materia("Anatomia")
+const naturales:Materia = new Materia("Naturales");
+const matematicas:Materia = new Materia("Matematicas");
+const fisica:Materia = new Materia("Fisica");
+const anatomia:Materia = new Materia("Anatomia");
 
 // Se crean las materias de segundo año:
-const algebra:Materia = new Materia("Algebra")
-const filosofia:Materia  = new Materia("Filosofia")
-const biologia:Materia  = new Materia("Biologia")
-const historia:Materia  = new Materia("Historia")
+const algebra:Materia = new Materia("Algebra");
+const filosofia:Materia  = new Materia("Filosofia");
+const biologia:Materia  = new Materia("Biologia");
+const historia:Materia  = new Materia("Historia");
 
 // Se crean las materias de tercer año:
-const geografia:Materia = new Materia("Geografia")
-const literatura:Materia  = new Materia("Literatura")
-const edFi:Materia  = new Materia("EdFi")
-const ingles:Materia  = new Materia("Ingles")
+const geografia:Materia = new Materia("Geografia");
+const literatura:Materia  = new Materia("Literatura");
+const edFi:Materia  = new Materia("EdFi");
+const ingles:Materia  = new Materia("Ingles");
 
 // Se crean las materias de cuarto año:
-const arte:Materia = new Materia("Arte")
-const economia:Materia  = new Materia("Economia")
-const finanzas:Materia  = new Materia("Finanzas")
-const quimica:Materia  = new Materia("Quimica")
+const arte:Materia = new Materia("Arte");
+const economia:Materia  = new Materia("Economia");
+const finanzas:Materia  = new Materia("Finanzas");
+const quimica:Materia  = new Materia("Quimica");
 
 //Se crean materias de quinto año:
-const tutoria:Materia = new Materia("Tutoria")
-const emprendedorismo:Materia  = new Materia("Emprendedorismo")
-const turismo:Materia  = new Materia("Turismo")
-const programacion:Materia  = new Materia("Programacion")
+const tutoria:Materia = new Materia("Tutoria");
+const emprendedorismo:Materia  = new Materia("Emprendedorismo");
+const turismo:Materia  = new Materia("Turismo");
+const programacion:Materia  = new Materia("Programacion");
 
 //Se crean materias de sexto año:
-const ntics:Materia = new Materia("Ntics")
-const fip:Materia  = new Materia("Fip")
-const administracion:Materia  = new Materia("Administracion")
-const comunicacion:Materia  = new Materia("Comunicacion")
+const ntics:Materia = new Materia("Ntics");
+const fip:Materia  = new Materia("Fip");
+const administracion:Materia  = new Materia("Administracion");
+const comunicacion:Materia  = new Materia("Comunicacion");
 
 // Guardamos las materias de cada curso en su respectivo arraydeMaterias:
 
@@ -55,12 +57,12 @@ const materiasDeQuinto:Materia[]=[tutoria,emprendedorismo,turismo,programacion];
 const materiasDeSexto:Materia[]=[ntics,fip,administracion,comunicacion];
 
 // Creamos los cursos con su "nombre" y las materias que lo componen:
-let primero:Curso= new Curso("Primero",materiasDePrimero)
-let segundo:Curso= new Curso("Segundo",materiasDeSegundo)
-let tercero:Curso= new Curso("Tercero",materiasDeTercero)
-let cuarto:Curso= new Curso("Cuarto",materiasDeCuarto)
-let quinto:Curso= new Curso("Quinto",materiasDeQuinto)
-let sexto:Curso= new Curso("Sexto",materiasDeSexto)
+let primero:Curso= new Curso("Primero",materiasDePrimero);
+let segundo:Curso= new Curso("Segundo",materiasDeSegundo);
+let tercero:Curso= new Curso("Tercero",materiasDeTercero);
+let cuarto:Curso= new Curso("Cuarto",materiasDeCuarto);
+let quinto:Curso= new Curso("Quinto",materiasDeQuinto);
+let sexto:Curso= new Curso("Sexto",materiasDeSexto);
 
 
 
@@ -73,7 +75,79 @@ export default class GestorColegio  {
 
 
     crearAlumno() {
-        // crear un alumno
+        let nombre:string = readLineSync.question(chalk.greenBright("Escriba el NOMBRE del alumno: ")).toLocaleLowerCase();
+        let apellido:string = readLineSync.question(chalk.greenBright("Escriba el APELLIDO del alumno: ")).toLocaleLowerCase();
+        let edad:number = readLineSync.question(chalk.greenBright("Escriba la EDAD del alumno: ")).toLocaleLowerCase();
+        let dni:number = readLineSync.question(chalk.greenBright("Escriba el DNI del alumno: ")).toLocaleLowerCase();
+
+        // creo un array para almacenar los NOMBRES de los cursos, los mismos son extraidos y pusheados(en "propiedadNombreDeLosCursos") con el ciclo for.
+        let propiedadNombreDeLosCursos:string[]=[]
+        for (let i = 0; i < this.cursos.length; i++) {
+            const curso = this.cursos[i];
+            propiedadNombreDeLosCursos.push(curso.nombre);
+        }
+        // muestro por consola el array "propiedadNombreDeLosCursos"(son los nombres de los cursos) para que usuario seleccione una opcion: 
+        let indiceCurso = readLineSync.keyInSelect(propiedadNombreDeLosCursos,"Seleccione el curso: ");
+        // guardo la opcion elegida en una variable que despues la recibe por parametro el constructor de la clase Alumno. 
+        const cursoElegido:Curso = this.cursos[indiceCurso]
+        console.clear();
+      
+        //se le asigna a matricula una id aleatoria de 7 caracteres:
+        let matricula:string = uuidv4().slice (0,7); 
+
+        // creo un array para almacenar los NOMBRES de LAS MATERIAS, los mismos son extraidos y pusheados(en "nombredeLasMaterias") con el ciclo for.
+
+        let nombredeLasMaterias:string[] = [];
+        for (let i = 0; i < cursoElegido.materias.length; i++) {
+            const materia = cursoElegido.materias[i];
+            nombredeLasMaterias.push(materia.nombre);
+        }
+        // creo un array para almacenar las materias que se matricula/anota el usuario por consola.
+        let materiasMatriculado:Materia[] = [];
+
+        // creo un while para que el usuario pueda repetir la seleccion de materias hasta que termine.
+        let condicion:number=0
+        while (condicion != -1) {
+        
+        let materia = readLineSync.keyInSelect(nombredeLasMaterias, "Seleccione las materias a las que se desea matricular (Ingrese 0 una vez que finalice): ");
+        // Condiciones para que no pueda anotarse mas de una vez a una misma materia y que no se almacena un dato erroneo en el array "materiasMatriculado"
+        if(materia!== -1 && nombredeLasMaterias[materia] != `---`){
+            materiasMatriculado.push(cursoElegido.materias[materia])
+            console.log(chalk.green(`Se ha matriculado a ${nombredeLasMaterias[materia]}`));
+            nombredeLasMaterias.splice(materia,1,"---")
+            }else {
+                console.log(chalk.red("Ya se matriculo a esa materia, seleccione una opcion valida"));
+            }
+        condicion=materia
+    }
+        console.clear();
+        
+        // creo un array donde se almacenaran los objetos "NotaPorMateria", los cuales tienen dos propiedades: nombre y nota.
+        const notasPorTodasLasMaterias: NotaPorMateria[]=[];
+        
+        // ciclo for para recorrer el array de materiasMatriculado del alumno:
+        for (let i = 0; i < materiasMatriculado.length; i++) {
+            // por cada iteracion le brindo el nombre de la materia, para que el usuario ingrese la nota de la misma:
+            let nota = Number(readLineSync.question(`Escriba la nota de la materia ${materiasMatriculado[i].nombre}: `));
+            // creo un objeto "notaPorMateria" que recibe por parametro el nombre de la materia(lo saco del array"materiasMatriculado:string") y la nota que le asigno anteriormente:
+            const nuevaNotaPorMateria: NotaPorMateria = new NotaPorMateria(materiasMatriculado[i],nota)
+            // hago un push de el objeto en el array creado al principio(notasPorTodasLasMaterias)
+            notasPorTodasLasMaterias.push(nuevaNotaPorMateria)
+        }
+
+
+        // en el acumulador se almacena la suma de todas las notas por materia y es dividida por la cantidad de las mismas para sacar el promedio, el cual se almacena en una variable.
+        let acumulador:number = 0
+        for (let i = 0; i < notasPorTodasLasMaterias.length; i++) {
+            acumulador = acumulador+ notasPorTodasLasMaterias[i].nota
+        }
+        let promedioGeneral:number = acumulador / notasPorTodasLasMaterias.length
+        
+
+        // creo el objeto alumno con todos los datos obtenidos anteriormente:
+        const nuevoAlumno: Alumno = new Alumno (nombre, apellido, edad, dni,matricula, cursoElegido, materiasMatriculado, notasPorTodasLasMaterias, promedioGeneral);
+        
+        return nuevoAlumno;
     }
 
     modificarAlumno() {
